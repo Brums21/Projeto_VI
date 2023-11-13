44dea2145b33c395668ed1309beb0040af603b28
Brums21 <75539238+Brums21@users.noreply.github.com>
2023-11-13 16:59:25 +0000

    fixed breaking

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer( (req, res) => {
+const server = http.createServer((req, res) => {
 
     let filePath = __dirname + '/pages/index.html';
 
@@ -20,10 +21,28 @@ const server = http.createServer( (req, res) => {
     }
 
     fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err.message);
+            res.statusCode = 500; // Internal Server Error
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Internal Server Error');
+            return;
+        }
+
+        const extname = path.extname(filePath);
+        let contentType = 'text/html';
+    
+        switch (extname) {
+          case '.js':
+            contentType = 'text/javascript';
+            break;
+          default:
+            break;
+        }
+
         res.statusCode = 200; // ok
-        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Type', contentType);
         res.end(data, 'utf8');
-        console.log(err.message);
     });
 
 });
---
+++ js/influx.js
@@ -0,0 +1,25 @@
+const Influxdb = require('influxdb-v2');
+
+const get_data_from_db = async (query_) => {
+  try {
+    const influxdb = new Influxdb({
+      host: '10.255.33.33', //might be necessary to add the port -> check later
+      token: 'M9Axugys0DnXwgkIb_SMc8Uaqf-0Hst4vbl44fV7IvY5c7yShUmjSlKwTWcUPpfsJpALW1DPxFiqgvZEglMB4Q=='
+    });
+
+    const result = await influxdb.query(
+      { org: 'b0fd31eef1cd0a40' },
+      { query: query_ }
+    );
+
+    console.log(result);
+
+    return result
+  }  
+  catch (error) {
+    console.error('\n An error occurred!', error);
+    throw error;
+  }
+}
+
+module.exports = get_data_from_db;
